4318db694aece2ca00e5bb10cb000514892f6997
www-data <www-data@calguy1000.com>
2020-02-26 19:18:01 +0000

    Added ability to provide a factory to create the response object.

--- src/Alexa.php
+++ src/Alexa.php
@@ -4,6 +4,8 @@ namespace Skollro\Alexa;
 
 use Exception;
 use Skollro\Alexa\Support\Pipeline;
+use Skollro\Alexa\Response;
+use Skollro\Alexa\Router;
 use MaxBeckers\AmazonAlexa\Request\Request;
 use Skollro\Alexa\Middleware\VerifyRequest;
 use Skollro\Alexa\Middleware\VerifyApplicationId;
@@ -13,6 +15,7 @@ class Alexa
     protected $router;
     protected $middlewares = [];
     protected $exceptionHandler;
+    protected $responseFactory;
 
     public function __construct()
     {
@@ -56,11 +59,30 @@ class Alexa
         return $this;
     }
 
+    public function responseFactory(callable $handler) : self
+    {
+        $this->responseFactory = $handler;
+        
+        return $this;
+    }
+
+    protected function createResponse() : Response
+    {
+        $out = null;
+        if( $this->responseFactory ) {
+            $out = $this->responseFactory;
+            if( is_callable($out) ) $out = call_user_func($out);
+        } else {
+            $out = new Response;
+        }
+        return $out;
+    }
+
     public function handle(Request $request, callable $callback = null)
     {
         try {
             $response = (new Pipeline)
-                ->pipe($request, new Response)
+                ->pipe($request, $this->createResponse())
                 ->through($this->middlewares)
                 ->then(function ($request, $response) {
                     return $this->router->dispatch($request, $response);
